7a6245bba4ef70c430c5f4981d9b6235b8fecb0f
Marcelo Brito <marcelogbrito@gmail.com>
2021-01-03 23:25:44 -0300

    instalado eslint e prettier

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const { response } = require('express')
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 const port = 3001
 
+app.use(express.static(path.join(__dirname,'./static')))
+
 app.get('/', (req, res) => {
-    res.send("Olá express")
+    res.sendFile(path.join(__dirname,'.static/index.html'))
+})
+
+app.get('/palestrantes', (req, res) => {
+    res.sendFile(path.join(__dirname,'.static/palestrantes.html'))
 })
 
 app.listen(port, () => {
